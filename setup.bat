6ad752e7ebb18770a979f086631e9981f7bdc636
YiPo <yipo.tw@hotmail.com>
2013-08-27 22:08:09 +0800

    Add Visual C++ 2010 Express (with .NET Framework 4).

--- setup.bat
+++ setup.bat
@@ -1,3 +1,10 @@
 @ECHO OFF
 
+ECHO ** Visual C++ 2010 Express with .NET Framework 4
+ECHO (about 30 minutes...)
+:: http://www.microsoft.com/visualstudio/cht/downloads#d-cpp-2010-express
+:: Run 'vc_web.exe /?' to show the command-line usage.
+
+vc2010e\vc_web.exe /q /norestart
+
 PAUSE
