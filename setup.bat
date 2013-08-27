f5fefe993b06a0b37a5811ee18385ac9acb7ee0d
YiPo <yipo.tw@hotmail.com>
2013-08-28 02:47:30 +0800

    Add error-checking.

--- setup.bat
+++ setup.bat
@@ -6,5 +6,20 @@ ECHO (about 30 minutes...)
 :: Run 'vc_web.exe /?' to show the command-line usage.
 
 vc2010e\vc_web.exe /q /norestart
+CALL:check
 
+
+:quit
 PAUSE
+EXIT
+
+
+:check
+IF ERRORLEVEL 1 (
+	ECHO [FAIL] %ERRORLEVEL%
+	GOTO quit
+) ELSE (
+	ECHO [OK] %TIME%
+)
+GOTO:EOF
+
