fdd04f440bbd21383dd1d1fe3c055a70930059fe
YiPo <yipo.tw@hotmail.com>
2013-09-01 01:08:06 +0800

    JDK: change back to silent mode.

--- pack/setup.bat
+++ pack/setup.bat
@@ -51,10 +51,11 @@ ECHO (about 5 minutes...)
 :: http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html
 :: Installing the JDK Silently
 :: http://docs.oracle.com/javase/7/docs/webnotes/install/windows/jdk-installation-windows.html#jdk-silent-installation
-:: Instead of '/s', '/passive' is used for showing the process bar.
-:: Run 'jdk-7u25-windows-i586.exe /?' to read the usage of parameters.
+:: // Instead of '/s', '/passive' is used for showing the process bar.
+:: // Run 'jdk-7u25-windows-i586.exe /?' to read the usage of parameters.
+:: Oh, no! '/passive' may somehow causes error 1603...
 
-jdk\jdk-7u25-windows-i586.exe /passive
+jdk\jdk-7u25-windows-i586.exe /s
 CALL:check
 
 
